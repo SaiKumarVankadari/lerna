9655ec86b33a072a4c62b1f5bcbfe3c7e343c8f2
Vankadari Sai Kumar <vankadarisaikumar@Vankadaris-MacBook-Air.local>
2025-05-22 14:34:47 +0530

    Initial setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { BackendModule } from './backend.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(BackendModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('LERNA')
+  .setDescription('Personal Project')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build()
+
+  const document =SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.port ?? 4000);
 }
 bootstrap();
